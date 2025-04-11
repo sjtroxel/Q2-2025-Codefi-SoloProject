6c8693b5c020f86d7871370fa45da25937837792
Steffan Troxel <sjtroxel@alumni.iu.edu>
2025-04-11 13:24:25 -0500

    outlined basic file structure

---
+++ q2MusicalInstrumentsSoloProject/src/app/features/templates/components/instrument-item/instrument-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instrument-item',
+  imports: [],
+  templateUrl: './instrument-item.component.html',
+  styleUrl: './instrument-item.component.css'
+})
+export class InstrumentItemComponent {
+
+}
---
+++ q2MusicalInstrumentsSoloProject/src/app/features/templates/components/instrument-list/instrument-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instrument-list',
+  imports: [],
+  templateUrl: './instrument-list.component.html',
+  styleUrl: './instrument-list.component.css'
+})
+export class InstrumentListComponent {
+
+}
